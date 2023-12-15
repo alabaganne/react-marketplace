4d45c059a5786c1eb87902a2b7302eb9e5b5a1c5
Ala Baganne <alabaganne9@gmail.com>
2023-12-15 09:20:56 +0100

    update App.jsx

--- client-app/src/main.jsx
+++ client-app/src/main.jsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
